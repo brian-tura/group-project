2a99ccfb32362893e32a0fab5b0f78da6be9ad47
brian-tura <b.tura2002@gmail.com>
2025-06-14 14:19:41 +0200

    minor fix

--- controllers/videogameController.js
+++ controllers/videogameController.js
@@ -1,43 +1,43 @@
 const connection = require('../data/db.js');
 
-function index(req, res){
+function index(req, res) {
     connection.query("SELECT * FROM videogames", (err, videogamesResult) => {
-        if(err) return res.status(500).json({error: "Database query failed"});
+        if (err) return res.status(500).json({ error: "Database query failed" });
 
-        res.status(200).json({success: true, data: videogamesResult})
+        res.status(200).json({ success: true, data: videogamesResult })
     })
 }
 
-function show(req, res){
+function show(req, res) {
     const id = req.params.id
 
     // queries
     const videogameQuery = `SELECT * FROM videogames WHERE id = ?`
-    const videogamePlatformsQuery = `
-        SELECT platforms.id, platforms.name
+
+    const videogamePlatformsQuery =
+        `SELECT platforms.id, platforms.name
         FROM videogame_platform
         JOIN platforms
         ON videogame_platform.platform_id = platforms.id
-        WHERE videogame_id = ?
-        `;
-    const videogamePublisherQuery = `
-        SELECT publishers.id, publishers.name
+        WHERE videogame_id = ?`;
+
+    const videogamePublisherQuery =
+        `SELECT publishers.id, publishers.name
         FROM videogames
         JOIN publishers
         ON videogames.publisher_id = publishers.id
-        WHERE videogames.id = 1
-    `;
-    const videogameGenresQuery = `
-        SELECT genres.id, genres.name
+        WHERE videogames.id = ?`;
+
+    const videogameGenresQuery =
+        `SELECT genres.id, genres.name
         FROM videogame_genre
         JOIN genres
         ON videogame_genre.genre_id = genres.id
-        WHERE videogame_genre.videogame_id = ?;
-    `;
+        WHERE videogame_genre.videogame_id = ?`;
 
     connection.query(videogameQuery, [id], (err, videogameResult) => {
-        if(err) return res.status(500).json({error: "Database query failed"});
-        
+        if (err) return res.status(500).json({ error: "Database query failed" });
+
         if (videogameResult.length === 0 || !videogameResult) {
             return res.status(404).send({
                 error: 'Not Found',
@@ -47,7 +47,7 @@ function show(req, res){
 
         connection.query(videogamePlatformsQuery, [id], (err, videogamePlatformResult) => {
 
-            if(err) return res.status(500).json({error: "Database query failed"})
+            if (err) return res.status(500).json({ error: "Database query failed" })
 
             videogameResult[0].platforms = videogamePlatformResult
 
@@ -55,7 +55,7 @@ function show(req, res){
 
         connection.query(videogamePublisherQuery, [id], (err, videogamePublisherResult) => {
 
-            if(err) return res.status(500).json({error: "Database query failed"})
+            if (err) return res.status(500).json({ error: "Database query failed" })
 
             videogameResult[0].publisher = videogamePublisherResult
 
@@ -63,16 +63,16 @@ function show(req, res){
 
         connection.query(videogameGenresQuery, [id], (err, videogameGenresResult) => {
 
-            if(err) return res.status(500).json({error: "Database query failed"})
+            if (err) return res.status(500).json({ error: "Database query failed" })
 
             videogameResult[0].genres = videogameGenresResult
 
             res.status(200).json(videogameResult)
         })
     })
-   
+
 }
 
 
 
-module.exports = {index, show}
+module.exports = { index, show }
