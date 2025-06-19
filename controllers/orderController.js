fed5d99714bb3858a5c700b151b87be11d09cf54
simonelupone <simone.lupone@gmail.com>
2025-06-20 01:47:34 +0200

    refactor: remove unused index function and enhance store function for order creation

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,72 +1,5 @@
 const connection = require("../data/db");
 
-function index(req, res) {
-  // come in vidogames
-
-  const ordersQuery = `
-    SELECT * FROM orders
-  `;
-
-  const discountsQuery = `
-    SELECT * FROM discounts
-  `;
-
-  const videogameOrdersQuery = `
-    SELECT 
-    videogame_order.order_id,
-    videogames.id AS videogame_id,
-    videogames.name AS videogame_name,
-    videogames.price AS videogame_price
-    FROM videogame_order
-    LEFT JOIN videogames ON videogame_order.videogame_id = videogames.id
-    WHERE videogame_order.order_id IN (?)
-  `;
-
-  connection.query(ordersQuery, (err, ordersResult) => {
-    if (err) return res.status(500).json({ error: "Database query failed" });
-
-    const ids = ordersResult.map((order) => order.id);
-
-    connection.query(discountsQuery, (err, discountsResult) => {
-      if (err) return res.status(500).json({ error: "Database query failed" });
-
-      connection.query(
-        videogameOrdersQuery,
-        [ids],
-        (err, videogamesOrderResult) => {
-          if (err)
-            return res.status(500).json({ error: "Database query failed" });
-
-          const compositeOrder = ordersResult.map((order) => {
-            const discount =
-              discountsResult.find(
-                (discount) => discount.id === order.discount_id
-              ) || null;
-
-            const videogames = videogamesOrderResult
-              .filter((videogame) => videogame.order_id === order.id)
-              .map((videogame) => ({
-                id: videogame.videogame_id,
-                name: videogame.videogame_name,
-                price: videogame.videogame_price,
-              }));
-
-            return {
-              id: order.id,
-              date: order.date,
-              status: order.status,
-              total_amount: order.total_amount,
-              discount: discount,
-              videogames: videogames,
-            };
-          });
-          res.status(200).json(compositeOrder);
-        }
-      );
-    });
-  });
-}
-
 function show(req, res) {
   const id = req.params.id;
 
@@ -124,4 +57,148 @@ function show(req, res) {
   });
 }
 
-module.exports = { index, show };
+function store(req, res) {
+  const { date, status, discount_id, videogames } = req.body;
+
+  // validation data
+  if (
+    !date ||
+    typeof status !== "boolean" ||
+    !Array.isArray(videogames) ||
+    videogames.length === 0
+  ) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  const ids = videogames.map((v) => v.id);
+
+  const orderVideogamesQuery = `
+    SELECT *
+    FROM videogames
+    WHERE id IN (?)
+  `;
+
+  connection.query(
+    orderVideogamesQuery,
+    [ids],
+    (err, orderVideogamesResult) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      if (orderVideogamesResult.length !== ids.length) {
+        return res.status(400).json({ error: "Some videogames not found" });
+      }
+
+      let totalAmount = orderVideogamesResult.reduce((total, videogame) => {
+        return total + Number(videogame.price);
+      }, 0);
+
+      console.log("amount before discount:", totalAmount);
+
+      // function to complete order
+      const completeOrder = (finalAmount, discountId = null) => {
+        const insertOrderQuery = `
+        INSERT INTO orders (date, status, total_amount, discount_id)
+        VALUES (?, ?, ?, ?)
+      `;
+
+        connection.query(
+          insertOrderQuery,
+          [date, status, finalAmount.toFixed(2), discountId],
+          (err, orderInsertResult) => {
+            if (err) {
+              console.error("Error inserting order:", err);
+              return res.status(500).json({ error: "Failed to create order" });
+            }
+
+            const orderId = orderInsertResult.insertId;
+
+            // prepear videogame_order association
+            const videogameOrderData = ids.map((videogameId) => [
+              videogameId,
+              orderId,
+              1,
+            ]);
+
+            // insert videogame_order association
+            const insertVideogameOrderQuery = `
+            INSERT INTO videogame_order (videogame_id, order_id, quantity)
+            VALUES ?
+          `;
+
+            connection.query(
+              insertVideogameOrderQuery,
+              [videogameOrderData],
+              (err, videogameOrderResult) => {
+                if (err) {
+                  console.error("Error inserting videogame orders:", err);
+                  return res.status(500).json({
+                    error: "Failed to associate videogames with order",
+                  });
+                }
+
+                // return created order
+                res.status(201).json({
+                  message: "Order created successfully",
+                  order: {
+                    id: orderId,
+                    date: date,
+                    status: status,
+                    total_amount: finalAmount.toFixed(2),
+                    discount_id: discountId,
+                    videogames: orderVideogamesResult.map((v) => ({
+                      id: v.id,
+                      name: v.name,
+                      price: v.price,
+                    })),
+                  },
+                });
+              }
+            );
+          }
+        );
+      };
+
+      // if discount_id -> apply
+      if (discount_id) {
+        const discountQuery = `
+        SELECT *
+        FROM discounts
+        WHERE id = ? AND (end_date IS NULL OR end_date >= CURDATE()) AND start_date <= CURDATE()
+      `;
+
+        connection.query(
+          discountQuery,
+          [discount_id],
+          (err, discountResult) => {
+            if (err) {
+              console.error("Error querying discount:", err);
+              return res.status(500).json({ error: "Database query failed" });
+            }
+
+            if (discountResult.length === 0) {
+              return res
+                .status(400)
+                .json({ error: "Invalid or expired discount code" });
+            }
+
+            const discount = discountResult[0];
+            const discountAmount = Number(discount.amount);
+
+            // apply discount
+            const finalAmount = totalAmount * (1 - discountAmount);
+
+            console.log("discount: ", discountAmount * 100 + "%");
+            console.log("final amount: ", finalAmount);
+
+            completeOrder(finalAmount, discount_id);
+          }
+        );
+      } else {
+        // if !discount -> base total
+        completeOrder(totalAmount);
+      }
+    }
+  );
+}
+
+module.exports = { show, store };
--- router/ordersRouter.js
+++ router/ordersRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const orderController = require("../controllers/orderController");
 
-router.get("/", orderController.index);
-
 router.get("/:id", orderController.show);
+
+router.post("/", orderController.store);
 
 module.exports = router;
