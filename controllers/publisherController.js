dc0d941716dfb40e37bbc412520d0aa27623b621
simonelupone <simone.lupone@gmail.com>
2025-06-20 17:27:26 +0200

    refactor: enhance publisher index function to include associated videogames

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -2,11 +2,30 @@ const connection = require("../data/db");
 
 function index(req, res) {
   const publishersQuery = "SELECT * FROM publishers";
+  const videogamesQuery = "SELECT * FROM videogames";
 
   connection.query(publishersQuery, (err, publishersResult) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
 
-    res.status(200).json(publishersResult);
+    connection.query(videogamesQuery, (err, videogamesResult) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      const publisherVideogames = publishersResult.map((publisher) => {
+        const publisherVideogames = videogamesResult
+          .filter((videogame) => videogame.publisher_id === publisher.id)
+          .map((videogame) => ({
+            ...videogame,
+            image: req.imagePath + videogame.image,
+          }));
+
+        return {
+          ...publisher,
+          videogames: publisherVideogames,
+        };
+      });
+
+      res.status(200).json(publisherVideogames);
+    });
   });
 }
 
